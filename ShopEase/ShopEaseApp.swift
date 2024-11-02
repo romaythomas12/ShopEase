3ee09a635e6b29e8c885ecc27c7f2406732d58ef
Thomas Romay <romaythomas@gmail.com>
2024-11-02 16:23:42 +0000

    Initial Commit

---
+++ ShopEase/ShopEaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopEaseApp.swift
+//  ShopEase
+//
+//  Created by Thomas Romay on 02/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopEaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
